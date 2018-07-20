8c6906854c232e29e71fd8ea97b8c6fb6c5080c2
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-20 11:15:15 -0700

    delete form to get rideofff reload, can also do by prevent default event, but just keep it simple

--- app.js
+++ app.js
@@ -2,4 +2,16 @@
 $(document).ready(function(){
 	// console.log('+++line2: fileName:app.js functionName:jquery.ready expected: no error actual : ',$)
 
+	// attach event listener to buttons(input)
+	// create function stub for read/write/delte
+		// research local storage
+
+	// $().on('click',function(){
+
+	// })
+	$('.store-btn').on('click',function(event){
+		console.log(event.target);
+		localStorage.setItem('hrext','there is the best')
+	})
+
 });
