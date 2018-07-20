aea23dc9229f68e359e88502799be7e1037f0288
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-20 10:32:11 -0700

    setting jdocument & jq

---
+++ app.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function(){
+	// console.log('+++line2: fileName:app.js functionName:jquery.ready expected: no error actual : ',$)
+
+});
---
+++ plan.js
@@ -0,0 +1,49 @@
+// Project Goals
+
+/*
+
+Accept Data - form, event listener, function to accept form data, input validation, different type of input, buttons.
+
+Store Data- accept validated form data and write to local storage.
+
+Retrieve Dta- button, function to grab storage, function to update display.
+
+Update Data-()
+
+Delete Data- remove data from local storage.
+
+
+
+MVP- A form with two fields, two buttons(one to get, one to set),able to get and set data in local 
+storage via the form. Basic UX and Nasic CSS.
+
+1.Create a form
+
+2.Add Event listener(s)
+
+3.Learn about local storage
+
+4.Implement basic write to local storage(hard code)
+
+5.implement basic read from local storage
+
+6.make read/write dynamic(attached to from event)
+6a. update data
+6b. delete data
+
+7.Basic CSS
+
+8.UX- Input confirmation/Validation
+
+
+MVP+
+	-Ability to filter list(live) via text
+	- Toastr messages- Inout confirmation/ success/ fail message
+	-Message local storage data based on purposed/ Intent
+	-Rebuilt Google Assistant
+	-Different Users
+
+*/
+
+
+
